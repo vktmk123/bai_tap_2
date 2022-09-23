5c1aa2759fc86e0927b616710c512ddd514a9378
Thanh <thanhvkgdh190702@fpt.edu.vn>
2022-09-24 01:44:44 +0700

    Sua Square

--- bai_tap_2/Inheritance/Program.cs
+++ bai_tap_2/Inheritance/Program.cs
@@ -12,22 +12,22 @@ namespace bai_tap_2
         static void Main(string[] args)
         {
             //bai 1////////////////////////////////////////////////////////////////////////////
-            ///* Test Point2D */
-            //// Test constructors and toString()
+            /* Test Point2D */
+            // Test constructors and toString()
             //Point2D p2a = new Point2D(1, 2);
             //Console.WriteLine(p2a); // toString()
             //Point2D p2b = new Point2D(); // default constructor
             //Console.WriteLine(p2b);
 
-            //// Test Setters and Getters
+            // Test Setters and Getters
             //p2a.X = 3; // Test setters
-            //p2a.Y = 4; 
+            //p2a.Y = 4;
             //Console.WriteLine(p2a); // toString()
             //Console.WriteLine("x is: " + p2a.X);
             //Console.WriteLine("x is: " + p2a.Y);
 
             /* Test Point3D */
-            // Test constructors and toString()
+            //Test constructors and toString()
 
             //bai 2////////////////////////////////////////////////////////////////////////////
             //Student s1 = new Student("Tan Ah Teck", "1 Happy Ave");
@@ -44,7 +44,7 @@ namespace bai_tap_2
             //Rectangle rectangle = new Rectangle(2,4);
             //Console.WriteLine(rectangle.ToString());
 
-            Square s = new Square(2);
+            //Square s = new Square(2);
             //Console.WriteLine(s.GetArea());
             //Console.WriteLine(s.GetPerimeter());
 
@@ -52,9 +52,9 @@ namespace bai_tap_2
             //Console.WriteLine(s.GetArea());
             //Console.WriteLine(s.GetPerimeter());
 
-            s.Length = 4;
-            Console.WriteLine(s.GetArea());
-            Console.WriteLine(s.GetPerimeter());
+            //s.Length = 4;
+            //Console.WriteLine(s.GetArea());
+            //Console.WriteLine(s.GetPerimeter());
 
             //s.Width = 5;
             //Console.WriteLine(s.GetArea());
--- bai_tap_2/Inheritance/Square.cs
+++ bai_tap_2/Inheritance/Square.cs
@@ -9,8 +9,16 @@ namespace bai_tap_2.Inheritance
     internal class Square : Rectangle
     {
         public double Side;
-        public override double Width { get; set; }
-        public override double Length { get; set; }
+        public override double Width
+        {
+            get { return Side; }
+            set { Side = value; }
+        }
+        public override double Length
+        {
+            get { return Side; }
+            set { Side = value; }
+        }
 
         public Square(double side) : base(side, side)
         {
@@ -21,23 +29,10 @@ namespace bai_tap_2.Inheritance
 
         public double GetArea()
         {
-            this.Length = this.Side;
-            this.Length = this.Width;
-            this.Width = this.Length;
-            this.Width = this.Side;
-            this.Side = this.Width;
-            this.Side = this.Length;
             return this.Side * Side;
         }
         public double GetPerimeter()
         {
-            this.Side = this.Width;
-            this.Side = this.Length;
-            this.Length = this.Side;
-            this.Length = this.Width;
-            this.Width = this.Length;
-            this.Width = this.Side;
-
             return this.Side * 4;
         }
 
